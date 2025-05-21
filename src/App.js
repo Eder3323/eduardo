76980d1427eeba41b28c02884c45305fc67f1413
Nubelity-Frontend-CoE <frontendcoe@nubelity.com>
2025-05-20 18:17:59 -0600

    Remove node_modules and add .gitignore

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Bienvenido Eduardo Valentín</h1>
+      <p>¡Nos emociona invitarte al baby shower!</p>
+      <ul>
+        <li><strong>Fecha:</strong> 22 de junio de 2025</li>
+        <li><strong>Hora:</strong> 4:00 PM</li>
+        <li><strong>Lugar:</strong> Casa de los abuelitos Tomás y Minerva, C. Durango, Col. Emiliano Zapata, Jaltepec</li>
+      </ul>
+      <iframe width="0" height="0" src="https://www.youtube.com/embed/xxJeIxsOl-Q?autoplay=1&loop=1&playlist=xxJeIxsOl-Q" 
+        title="Phil Collins - En mi corazón vivirás" 
+        frameBorder="0" allow="autoplay" allowFullScreen></iframe>
+    </div>
+  );
+}
+
+export default App;
